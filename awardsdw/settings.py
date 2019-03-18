45a9a3233e3f8116a0b80b69ac4b1356600fd6a7
derrrick william <williamderrick100@gmail.com>
2019-03-18 13:45:40 +0300

    Added models.

--- awardsdw/settings.py
+++ awardsdw/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'trophy',
     'bootstrap4',
-    'accounts'
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- trophy/models.py
+++ trophy/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Projects(models.Model):
+
+    projo_pic = models.ImageField(upload_to='projo-pic/')
+    title = models.CharField(max_length=50)
+    link = models.URLField(max_length=250)
+    user = models.ForeignKey(User,null=True)
+
+class Profile(models.Model):
+
+    profile_pic = models.ImageField(upload_to='profile-pic/')
+    bio = models.TextField()
+    phone_number = models.IntegerField()
+
+class Rating(model.Model):
+    design = models.IntegerField()
+    usability = models.IntegerField()
+    content = models.IntegerField()
+    
+
+
+
+
